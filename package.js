e2cef4cbb87f6cee082b2b36f318b9cd80207c9e
Kevin O'Hagan <kevinohagan@gmail.com>
2015-02-11 16:28:06 +0100

    updated to 0.9.1.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'kevohagan:ramda',
     summary: 'A practical functional library for Javascript programmers.',
-    version: '0.9.1',
+    version: '0.9.1.1',
     git: 'https://github.com/kevohagan/meteor-ramda.git',
     upstreamVersion: "0.1.0"
 });
@@ -11,9 +11,7 @@ Package.onUse(function(api) {
     api.versionsFrom("METEOR@0.9.1.1");
 
     api.add_files([
-        'ramda/dist/ramda.js',
+        'ramda/dist/ramda.min.js',
     ], ['client', 'server']);
 
-    if(api.export)
-        api.export('R');
 })
