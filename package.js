cf0af9bdc2f47e74e6a6e6a5695f87117c14bfc1
Kevin O'Hagan <kevinohagan@gmail.com>
2015-03-31 16:23:34 +0200

    updated to 0.13

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'kevohagan:ramda',
     summary: 'A practical functional library for Javascript programmers.',
-    version: '0.12.0',
+    version: '0.13.0',
     git: 'https://github.com/kevohagan/meteor-ramda.git',
     upstreamVersion: "0.1.0"
 });
